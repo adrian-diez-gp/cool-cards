52b8d3099e65e84d89c92485886e06bbc024a563
Adrian <adrian.diez@solera.com>
2023-09-14 12:12:44 +0200

    starting new design

--- src/White.js
+++ src/White.js
@@ -7,6 +7,27 @@ export default function white() {
 
     return (
         <div className="colour-page-wrapper">
+            <h1>A (Always get them)</h1>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/expeltheinterlopersf.jpg" />
+                <p><strong>A-</strong>. Good in a tokens deck (to get rid of everything bigger than your creatures) or in a control deck.</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/atalefortheagesf.jpg" />
+                <p><strong>A-</strong>. It alone doesn't do anything. Verrry few decks don't want this. Roles also count, so start picking those up more.</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/archonofthewildrosef.jpg" />
+                <p><strong>A</strong>.  Almost worth giving up a colour and going into white. As good or more as the enchantment, but it alone is already great!</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/virtueofloyalty.jpg" />
+                <p><strong>A+</strong>.  A is for Amazing here. 2/2 at instant speed is awesome, and cumulative bonus on your creatures is the cherry on top!</p>
+            </div>
+            
+            <h1>B (Be looking for these!)</h1>
+
+
             <h1>COMMON CARDS</h1>
             <img src="https://mythicspoiler.com/woe/cards/slumberingkeepguard.jpg" />
             <img src="https://mythicspoiler.com/woe/cards/saviorofthesleeping.jpg" />
@@ -20,9 +41,8 @@ export default function white() {
             <img src="https://mythicspoiler.com/woe/cards/theprincesstakesflight.jpg" />
 
             <h1>RARES AND MYTHICS</h1>
-            <img src="https://mythicspoiler.com/woe/cards/expeltheinterlopersf.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/archonofthewildrosef.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/atalefortheagesf.jpg" />
+            <img src="" />
+
         </div>
     )
 }
