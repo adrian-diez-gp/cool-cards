4c60684d883effaf10e7d4314d01795de9e70e72
Adrian <adrian.diez@solera.com>
2023-09-14 12:28:59 +0200

    white update

--- src/White.js
+++ src/White.js
@@ -10,23 +10,54 @@ export default function white() {
             <h1>A (Always get them)</h1>
             <div>
                 <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/expeltheinterlopersf.jpg" />
-                <p><strong>A-</strong>. Good in a tokens deck (to get rid of everything bigger than your creatures) or in a control deck.</p>
+                <p><strong>Rating: A-</strong>. Good in a tokens deck (to get rid of everything bigger than your creatures) or in a control deck.</p>
             </div>
             <div>
                 <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/atalefortheagesf.jpg" />
-                <p><strong>A-</strong>. It alone doesn't do anything. Verrry few decks don't want this. Roles also count, so start picking those up more.</p>
+                <p><strong>Rating: A-</strong>. It alone doesn't do anything. Verrry few decks don't want this. Roles also count, so start picking those up more.</p>
             </div>
             <div>
                 <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/archonofthewildrosef.jpg" />
-                <p><strong>A</strong>.  Almost worth giving up a colour and going into white. As good or more as the enchantment, but it alone is already great!</p>
+                <p><strong>Rating: A</strong>.  Almost worth giving up a colour and going into white. As good or more as the enchantment, but it alone is already great!</p>
             </div>
             <div>
                 <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/virtueofloyalty.jpg" />
-                <p><strong>A+</strong>.  A is for Amazing here. 2/2 at instant speed is awesome, and cumulative bonus on your creatures is the cherry on top!</p>
+                <p><strong>Rating: A+</strong>.  A is for Amazing here. 2/2 at instant speed is awesome, and cumulative bonus on your creatures is the cherry on top!</p>
             </div>
             
             <h1>B (Be looking for these!)</h1>
-
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/theprincesstakesflight.jpg" />
+                <p><strong>Rating: B+</strong>. Chapter 1 gets rid of an enemy creature; chapter 2 gives you a good attack. If you BARGAIN this before chapter 3, the creature doesn't come back!</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/pollenshieldhare.jpg" />
+                <p><strong>Rating: B-</strong>. A good bonus for tokens, it's a 2/2 if you have nothing better to do in turn 2. Obviously better if you can play the green part!</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/heartflameduelistf.jpg" />
+                <p><strong>Rating: B-</strong>.  A tricky one here. Pick it ONLY if you're already in red (not necessarily white), as that is the good side of this card.</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/regalbunnicornf.jpg" />
+                <p><strong>Rating: B-</strong>.  Can it die? Yes. Will you play it in turn 2? Probably. Can this get huge quickly with the help of food tokens etc? Absolutely!</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/werefoxbodyguardf.jpg" />
+                <p><strong>Rating: B-</strong>.  A FLASHY rare that more often than not will get rid of a big guy for a few turns before it dies</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/threeblindmicef.jpg" />
+                <p><strong>Rating: B-</strong>.  A payoff for all those token cards! It's rare so you probably won't see it though.</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/shroudedshepherd.jpg" />
+                <p><strong>Rating: B</strong>.  Pick it in a nonblack deck, you get a niiice attack. Pick it in a black deck, and you can debilitate your opponent's creatures before an attack. Play it against a Rat deck and, well... you might like that game. :)</p>
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/hopefulvigil.jpg" />
+                <p><strong>Rating: B</strong>.  Normally you don't want multiples of your cards in your deck. But if your 2 mana slot is filled with 3 of these, I'd call that a good deck. Reaaaally good for bargaining, or for those token bonuses!</p>
+            </div>
 
             <h1>COMMON CARDS</h1>
             <img src="https://mythicspoiler.com/woe/cards/slumberingkeepguard.jpg" />
@@ -38,10 +69,10 @@ export default function white() {
             <img src="https://mythicspoiler.com/woe/cards/strokeofmidnightf.jpg" />
             <img src="https://mythicspoiler.com/woe/cards/knightofdovesf.jpg" />
             <img src="https://mythicspoiler.com/woe/cards/solitarysanctuary.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/theprincesstakesflight.jpg" />
+            
 
-            <h1>RARES AND MYTHICS</h1>
-            <img src="" />
+           
+           
 
         </div>
     )
