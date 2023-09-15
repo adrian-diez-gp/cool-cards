6bcde106e1d332011563455b1fa44a15450cc0ed
Adrian <adrian.diez@solera.com>
2023-09-15 09:40:03 +0200

    starting green

--- src/Green.js
+++ src/Green.js
@@ -7,6 +7,85 @@ export default function white() {
 
     return (
         <div className="colour-page-wrapper">
+            <h1>A (Always get them)</h1>    
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/elvisharchivistf.jpg"/>
+                <p><strong>Rating: A</strong>. This needs some setup to be goo-HAHAHA! Look at how many cards in this set
+                put artifacts and/or enchantments into play, and you'll realise that this is a BEAST. Drop a color if you see one,
+                you're now green/X.</p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/elvisharchivistf.jpg"/>
+                <p><strong>Rating: A+</strong>. Literally the only time I wouldn't pick this is pack 3. This is 3 3/3, that
+                become two 6/6, that becomes a 12/12, all with trample. WHAT?! Imagine if you could recur it from the graveyard... e.e</p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/mosswooddreadknightf.jpg"/>
+                <p><strong>Rating: A-</strong>. Slow, but can become a wall if you need it. This blocks X/3, trades, then gets you a card and comes back (if you get a way to
+                get black mana).</p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/gingerbreadhunterf.jpg"/>
+                <p><strong>Rating: A-</strong>. A reason to look for black mana. Two real cards in one? And it's a cost 5 to boot! All the sinergies are there!</p>
+                <br />
+            </div> 
+
+            <h1>B (Be looking for these!)</h1>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/sentineloflostloref.jpg"/>
+                <p><strong>Rating: B</strong>. The power/toughness/mana ratio is there for the taking, but only if you're already green. Generally, the more 
+                powerful cards you get, the better (surprise surprise!)</p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/agathaschampionf.jpg"/>
+                <p><strong>Rating: B-</strong>.What is this? A 5 mana card? This one is also a removal spell if you can bargain something away!</p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/gracefultakedownf.jpg"/>
+                <p><strong>Rating: B+</strong>.If you don't have bargain fodder but you have creatures (and being in green, you should), this is your alternative.
+                You'll almost always trade up in mana</p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/welcometosweettoothf.jpg"/>
+                <p><strong>Rating: B</strong>.If you need bargain fodder, this is your card. Literally an enchantment, an artifact, and a token in one card.
+                Do you think they did it on purpose?</p>
+                <br />
+            </div>
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/upthebeanstalkf.jpg"/>
+                <p><strong>Rating: B+</strong>. For 2 mana, it replaces itself, so you only need one or two 5 mana cards to start paying off. And LOOOTS of 
+                5 mana cards have a cheaper adventure you can play earlier. Combo, anyone?</p>
+                <br />
+            </div>  
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/toughcookief.jpg"/>
+                <p><strong>Rating: B+</strong>. Gingerbread man is angry that you played with his buttons. This can hit for 6 in turn 3, and further
+                 foods can make him that much stronger. </p>
+                <br />
+            </div> 
+            <div>
+                <img style={{display:"block"}} src="https://mythicspoiler.com/woe/cards/stormkeldvanguardf.jpg"/>
+                <p><strong>Rating: B-</strong>. Imagine you want a card that destroys artifacts and enchantments. But that card is not always good, so you leave it in the sideboard. 
+                But then you reaaaally need it in your game 1. THIS is your solution to that problem. If you draw it late, no problem -just play a huge dumdum that can't
+                be chumpblocked!</p>
+                <br />
+            </div>
+            
+            
+            <h1>C (Cards that will make the cut, or that need support from other cards)</h1>
+
+            https://mythicspoiler.com/woe/cards/thehuntsmansredemptionf.jpg
+            https://mythicspoiler.com/woe/cards/questingdruidf.jpg
+            https://mythicspoiler.com/woe/cards/bramblefamiliarf.jpg
+            https://mythicspoiler.com/woe/cards/feralencounterf.jpg
+            https://mythicspoiler.com/woe/cards/tempesthart.jpg
+
             <h1>COMMON CARDS</h1>
             <img src="https://mythicspoiler.com/woe/cards/returnfromthewildsf.jpg" />
             <img src="https://mythicspoiler.com/woe/cards/hollowscavengerf.jpg" />
@@ -18,13 +97,9 @@ export default function white() {
             <img src="https://mythicspoiler.com/woe/cards/toughcookief.jpg" />
             <img src="https://mythicspoiler.com/woe/cards/howlinggalefangf.jpg" />
             <img src="https://mythicspoiler.com/woe/cards/stormkeldvanguardf.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/nightofthesweetsrevengef.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/welcometosweettoothf.jpg" />
+
 
             <h1>RARES AND MYTHICS</h1>
-            <img src="https://mythicspoiler.com/woe/cards/grufftripletsf.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/elvisharchivistf.jpg" />
-            <img src="https://mythicspoiler.com/woe/cards/mosswooddreadknightf.jpg" />
         </div>
     )
 }
