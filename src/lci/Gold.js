91b2751da92959d784c69dfebd81fdc057bfa22d
Adrian <adrian.diez@solera.com>
2023-11-10 09:12:17 +0100

    gold started

--- src/lci/Gold.js
+++ src/lci/Gold.js
@@ -10,134 +10,14 @@ export default function Gold() {
     return (
         <div className="colour-page-wrapper">
             <div onClick={() => navigate('/')}><b>Home</b></div>
+            <div onClick={() => navigate('/lci')}><b>Back</b></div>
+            
+            <h3>
+                For multicolor cards, refer to <a href="https://www.mythicspoiler.com">Mythicspoiler</a>, as they're all great!
+            </h3>
 
-           
-            <h1>GOLD CARDS,ARTIFACTS, AND LANDS</h1>
-            <br />
-            <h1>A (Archetype defining)</h1>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/agathassoulcauldronf.jpg"/>
-                <p><strong>Rating: C+++</strong>. If this is your first card, get it. Never sad to get "free" +1/+1 counters. From the third on,
-                this card is overperforming.</p>
-                <p><strong>EDIT: This card is 30 euros. You open it, we both win. OPEN IT!!!!!</strong></p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/hyldaoftheicycrownf.jpg"/>
-                <p><strong>Rating: A</strong>. Innncredible if you find enough support. According to experts, blue/white is on the decline, so
-                you'll find more support cards than normal to go with this.</p>
-                <br />
-            </div> 
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/sharaeofnumbingdepthsf.jpg"/>
-                <p><strong>Rating: A-</strong>. When everything that taps gives you a card, life is a little brighter. You need this in play tho (Play-doh!).</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/erietteofthecharmedapplef.jpg"/>
-                <p><strong>Rating: A-</strong>. Love it. This plus a couple roles win you the game in a few turns. Cursed roles on opponent's creatures also count!</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/talionthekindlylord.jpg"/>
-                <p><strong>Rating: A+</strong>. Mythic cards are coming strong!! In the dark, choose 2 or 3. Then look at your opp's cards and decide which is better.
-                Also, it's a faerie!</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/thegoosemotherf.jpg"/>
-                <p><strong>Rating: A+</strong>. This mama will cook you dinner if you wish for it hard enough. It is never a bad time to cast it
-                (except turn 2 of course!)</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/faunsbanetrollf.jpg"/>
-                <p><strong>Rating: A-</strong>.Play it, fight something (as a 4/4!), put a role on it, repeat. :D</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/gretasweettoothscourgef.jpg"/>
-                <p><strong>Rating: A-</strong>. One of the two gold A's at uncommon. Such versatile, much wow. Also, 3/3 for 3 :DD</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/hyldascrownofwinterf.jpg"/>
-                <p><strong>Rating: A+</strong>. If you open this, shout loud so I can cry. Good when you're behind, even better when you're ahead.
-                Honestly, I don't know what the fuck they were thinking when they printed this card.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/restlessfortressf.jpg"/>
-                <p><strong>Rating: A-</strong>. All the manlands are good if you're in those colours. If not, don't bother.</p>
-                <br />
-            </div>
-            <h1>B (Pick at least one if you find them and in your colours)</h1>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/evolvingwilds.jpg"/>
-                <p><strong>Rating: B</strong>. Mana fixing is one of the most powerful things you can do in magic, behind drawing cards and juust above winning. Get the first ALWAYS.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/propheticprismf.jpg"/>
-                <p><strong>Rating: B+</strong>. I put these here to stress how important mana fixing is. This in particular gives you a card and can be 
-                bargained away later in the game, so again, pick one if you're going for off-colour adventures.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/troyangutsyexplorerf.jpg"/>
-                <p><strong>Rating: B-</strong>. This throws you straight into the 5 mana territory, which is something that you wanna do in these colours,
-                and loots away extra lands or tries to find an answer. Solid one.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/obyradreamingduelist.jpg"/>
-                <p><strong>Rating: B</strong>. If you're in these colours, chances are you're in faeries, so the more of these you pick, the 
-                happier you'll be.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/totentanzswarmpiperf.jpg"/>
-                <p><strong>Rating: B</strong>. You in these colurs? You pick this. You'll find support in your deck, no worries. You can also
-                start attacking recklessly to grow your rat collection!</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/nevastalkedbynightmaresf.jpg"/>
-                <p><strong>Rating: B</strong>. Again, if you're set in these colours, this is an amazing addition to the deck.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/johannapprenticesorcererf.jpg"/>
-                <p><strong>Rating: B</strong>. Read above. (This lets you cast adventures from the top, they're not instant cards but they're instant spells!)</p>
-                <br />
-            </div>
+            <div style={{color:"blue"}} onClick={() => navigate('/lci')}><b>Back to colour selection</b></div>
 
-            <h1>C (Good fillers)</h1>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/candytrailf.jpg"/>
-                <p><strong>Rating: C+</strong>. I'll never be sad to have one of these in the deck. Provides card selection, life, bargain fodder
-                and replaces itself if you sac it. A true all-in-one!</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/edgewallinnf.jpg"/>
-                <p><strong>Rating: C</strong>. Mana fixing for your adventure color, but you need to sac it to get back the adventure. Hm. Maybe if the creature side is good enough.
-                Maybe.</p>
-                <br />
-            </div>
-
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/collectorsvaultf.jpg"/>
-                <p><strong>Rating: C</strong>. In a long game, this can net you three extra cards PLUS three treasures (sneaky mana fixing!) 
-                Put this in as your 22-23rd card.</p>
-                <br />
-            </div>
-            <div>
-                <img className='card-wrapper' src="https://mythicspoiler.com/woe/cards/eriettestemptingapplef.jpg"/>
-                <p><strong>Rating: C-</strong>. An interesting effect in colourless. Always best if you can sacrifice the creature before giving 
-                it back to its rightful owner.</p>
-                <br />
-            </div>
         </div>
     )
 }
